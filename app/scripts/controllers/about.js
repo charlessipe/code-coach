ebb2bb588ed0b21f14b0b49797a1602b1bac6967
Charles Sipe <csipe84@gmail.com>
2016-10-05 06:43:43 -0700

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rankedResourcesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rankedResourcesApp
+ */
+angular.module('rankedResourcesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rankedResourcesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rankedResourcesApp
+ */
+angular.module('rankedResourcesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
