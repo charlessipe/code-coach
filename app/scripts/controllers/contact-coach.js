195742313780099c7d24b1237419b5397b752ea8
Charles Sipe <csipe84@gmail.com>
2016-11-23 17:32:17 -0800

    Automatically add email in contact coach form

--- app/scripts/controllers/contact-coach.js
+++ app/scripts/controllers/contact-coach.js
@@ -10,13 +10,28 @@
 angular.module('rankedResourcesApp')
   .controller('ContactCoachCtrl', function ($scope) {
     
+    $scope.checkLoginStatus = function(){
+      firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          console.log(user);
+          $scope.currentUserEmail = user.email;
+          $scope.currentUserId = user.uid;
+          console.log($scope.currentUserEmail);
+          console.log($scope.currentUserId);
+          //$scope.permissions(); // call permissions function
+          $scope.$apply();
+        } else {
+          console.log("Please log into your account");
+        }
+      });
+    }() // end checkLoginStatus 
 
     $scope.contactCoach = function (){
       firebase.database().ref('messages/' + Date.now()).set({
         coachName: $scope.coachName,
         firstName: $scope.firstName,
         lastName: $scope.lastName,
-        email: $scope.newEmail,
+        email: $scope.currentUserEmail,
         message: $scope.newMessage
       });  
 
@@ -27,6 +42,8 @@ angular.module('rankedResourcesApp')
       $scope.newEmail = "";
     
     } // end contactCoach
+
+    
 
 
 
