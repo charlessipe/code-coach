f6ed850a516db8b48a2191f88d268758d0953f06
Charles Sipe <csipe84@gmail.com>
2016-11-29 13:27:03 -0800

    Fixed saveCoach so adds coach name to favs when no dup

--- app/scripts/controllers/javascript-coaches.js
+++ app/scripts/controllers/javascript-coaches.js
@@ -47,6 +47,25 @@ angular.module('rankedResourcesApp')
       //console.log("Coach1: " + $scope.users["andyj"].firstName + " " + $scope.users["andyj"].lastName + " " + $scope.users["andyj"].bio);
     });
 
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        $scope.currentUserEmail = user.email;
+        $scope.currentUserId = user.uid;
+        //$scope.permissions(); // call permissions function
+        
+        // retrieve favorites from logged in user, and also when new favorites are added
+        var favoriteData = firebase.database().ref('students/' + $scope.currentUserId + '/favorites/');
+        favoriteData.on('value', function(snapshot) {
+          $scope.favoriteList = snapshot.val();
+          console.log('$scope.favoriteList in once', $scope.favoriteList);
+        });  
+
+        $scope.$apply();
+      } else {
+    }
+  });
+
+
 
   $scope.checkLoginStatus = function(){
       firebase.auth().onAuthStateChanged(function(user) {
@@ -66,48 +85,32 @@ angular.module('rankedResourcesApp')
 
   // Save coach name to Firebase db when button clicked
   $scope.saveCoach = function(index){
+    var coach = $scope.usersArray[index];
+    var coachName = coach.firstName + " " + coach.lastName;
+    alert(coachName); // alert full name of coach
 
-    console.log("Coach number " + index + " added");
-    alert("Coach number " + index + " added");
-    alert($scope.currentUserId);
-    alert($scope.usersArray[index].firstName + " " + $scope.usersArray[index].lastName); // get first name of coach
-  
-    /*var favoriteData = firebase.database().ref('students/' + $scope.currentUserId + '/favorites/');
-        favoriteData.on('value', function(snapshot) {
-          $scope.favoriteList = snapshot.val();
-          console.log($scope.favoriteList);
-    });*/
-
-    // retrieve favorites from logged in user
-    var favoriteData = firebase.database().ref('students/' + $scope.currentUserId + '/favorites/');
-    
-    favoriteData.once('value').then(function(snapshot) {
-      $scope.favoriteList = snapshot.val();
-      console.log($scope.favoriteList); 
-    });  
-  
-    
-    setTimeout(function(){ 
-    console.log($scope.favoriteList);
-
+    var isNameInList = false;
     for (var key in $scope.favoriteList) {
-      console.log($scope.favoriteList[key].name);
-
-      if (($scope.usersArray[index].firstName + " " + $scope.usersArray[index].lastName) == $scope.favoriteList[key].name){
+      if (coachName == $scope.favoriteList[key].name){
         console.log("Duplicate found!");
+        console.log($scope.favoriteList[key].name);
+        isNameInList = true;
       }
-      else {
-        console.log("No duplicate");
-        // set Firebase coach's name
-        // store favorite coach name in Firebase db
-        firebase.database().ref('students/' + $scope.currentUserId + '/favorites/' + Date.now()).set({
-          name: $scope.usersArray[index].firstName + " " + $scope.usersArray[index].lastName,
-          favoriteDate: moment.unix(Date.now()).format("MMM DD h:mm A")
-          }); 
-      } 
-    }  // end for loop
-    }, 1000); // end setTimeout
+    } 
+    // end for loop
 
+    // only set name and store in DB if 'isNameInList' is false
+    if (!isNameInList) {
+    // set Firebase coach's name
+    // store favorite coach name in Firebase db
+      firebase.database().ref('students/' + $scope.currentUserId + '/favorites/' + Date.now()).set({
+        name: coachName,
+        favoriteDate: moment.unix(Date.now()).format("MMM DD h:mm A")
+      }); 
+    }
+    else {
+      alert('not gonna add, duplicate');
+    }
 
   } // end saveCoach
 
